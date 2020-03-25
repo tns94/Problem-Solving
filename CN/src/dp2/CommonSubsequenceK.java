1b8c30c5fc7bfaf3aee9be664abe2647d6396f1d
Tarun Sharma <tarun10994@gmail.com>
2020-03-26 00:42:17 +0530

    DP CommonSubsequenceK

--- CN/src/dp2/CommonSubsequenceK.java
+++ CN/src/dp2/CommonSubsequenceK.java
@@ -13,62 +13,43 @@ public class CommonSubsequenceK {
             int t=sc.nextInt();
             String str1;
             String str2;
-            int k;
+            int K;
             for (int l = 0; l < t; l++) {
                 str1=sc.next();
                 str2=sc.next();
-                k=sc.nextInt();
+                K=sc.nextInt();
 
                 int m = str1.length();
                 int n = str2.length();
 
-                int[][] dp = new int[m + 1][n + 1];
-
-                String[][] dps = new String[m + 1][n + 1];
-                for (int i = 0; i <= n; i++) {
-                    dps[0][i] = "";
-                }
-                for (int i = 0; i <= m; i++) {
-                    dps[i][0] = "";
-                }
+                int[][] dp1=new int[m+1][n+1];
+                int[][][] dp = new int[m + 1][n + 1][K+1];
 
                 for (int i = 1; i <= m; i++) {
                     for (int j = 1; j <= n; j++) {
-                        if (str1.charAt(m - i) == str2.charAt(n - j)) {
-                            dp[i][j] = dp[i - 1][j - 1] + 1;
-                            dps[i][j] = str1.charAt(m - i) + dps[i - 1][j - 1];
-                        } else if (dp[i - 1][j] > dp[i][j - 1]) {
-                            dp[i][j] = dp[i - 1][j];
-                            dps[i][j] = dps[i - 1][j];
-                        } else {
-                            dp[i][j] = dp[i][j - 1];
-                            dps[i][j] = dps[i][j - 1];
-                        }
+
+                            if(str1.charAt(m-i)==str2.charAt(n-j)){
+                                dp1[i][j]=dp1[i-1][j-1]+1;
+                                for (int k = 1; k <= K; k++)
+                                    dp[i][j][k]=Math.max(dp[i-1][j-1][k-1]+(int) str1.charAt(m-i),Math.max(dp[i-1][j][k],dp[i][j-1][k]));
+                            }
+                            else
+                            {
+                                dp1[i][j]=dp1[i-1][j]>dp1[i][j-1]?dp1[i-1][j]:dp1[i][j-1];
+                                for (int k = 1; k <= K; k++)
+                                dp[i][j][k]=Math.max(dp[i-1][j][k],dp[i][j-1][k]);
+                            }
+
+
                     }
+
                 }
 
-                int sum=0;
-                String s = dps[m][n];
-                System.out.println(s);
-                if(!(s.length()<k)) {
-                    int[] arr = new int[26];
 
-                    for (int i = 0; i < s.length(); i++) {
-                        arr[ s.charAt(i)-97]++;
-                    }
-
-
-                    for (int i = 25; i >=0;i--) {
-                        if (arr[i]!=0)
-                        {
-                            sum+=(i+97)*(Math.min(arr[i],k));
-                            k-=arr[i];
-                            if (k<=0)
-                                break;
-                        }
-                    }
-                }
-                System.out.println(sum);
+                if (dp1[m][n]<K)
+                    System.out.println(0);
+                else
+                    System.out.println(dp[m][n][K]);
 
             }
     }
