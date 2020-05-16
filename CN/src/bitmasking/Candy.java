c809ca0b260d9f64c580b2c3011a2e5ce9c734de
Tarun Sharma <tarun10994@gmail.com>
2020-05-16 19:05:21 +0530

    BitMasking Candy

--- CN/src/bitmasking/Candy.java
+++ CN/src/bitmasking/Candy.java
@@ -3,28 +3,32 @@ package bitmasking;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
-
+/*
+N integers describing the preferences of one student. 1 at i'th (0 <= i < N) position denotes that this student likes i'th candy , 0 means he doesn't.
+Return the number of ways one can distribute these N candies to his N students such that every student gets exactly one candy he likes.``
+ */
 public class Candy {
 
     static long solve(int[][] like,int N){
         long[] dp=new long[1<<N];
-        boolean[] dph=new boolean[1<<N];
-        return solve(dp,dph,like,0,0,N);
+        for (int i = 0; i < 1 << N; i++) {
+            dp[i]=-1;
+        }
+        return solve(dp,like,0,0,N);
     }
-    static long solve(long[] dp,boolean[] dph,int[][] input,int mask,int stu,int n)
+    static long solve(long[] dp,int[][] input,int mask,int stu,int n)
     {
         if(n==stu)
             return 1;
-        if(dph[mask])
+        if(dp[mask]!=-1)
             return dp[mask];
         long co=0;
         for (int i = 0; i < n; i++) {
             if(input[stu][i]!=0 && (mask&(1<<i))==0)
             {
-                co+=solve(dp,dph,input,mask|(1<<i),stu+1,n);
+                co+=solve(dp,input,mask|(1<<i),stu+1,n);
             }
         }
-        dph[mask]=true;
         dp[mask]=co;
         return co;
     }
